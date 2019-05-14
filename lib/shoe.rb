a5f24b264af2b325d0baba3c2b2dc80263e4491b
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 22:32:58 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand) # checks if brand is already in []
+      BRANDS # returns original array if this is true
+    else
+      BRANDS << brand 
+    end
   end
 
   def cobble
